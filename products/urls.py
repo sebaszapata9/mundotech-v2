48cf156c24ea94d1975cb4b4b32a6ce195ede793
sebas123 <zapatavergara28@gmail.com>
2025-08-06 13:17:01 -0500

    actualizacion json1

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('api/GetPhones',views.api_get_phones)
 ]
 
 app_name = 'products'
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Phone  # Importing the Phone model from models.py
+from django.core import serializers
 
 # Create your views here.
 
@@ -12,3 +13,9 @@ def index(request):
     phones = Phone.objects.all().order_by('price')  # Fetch all phone objects from the database
   
     return render(request, 'products/index.html', {'phones': phones})  # Render the index template
+
+def api_get_phones(request):
+	phones = Phone.objects.all().order_by('price')
+	json = serializers.seralize("json",pizzas) #este es el string que
+										# vamos a pasar en httpresponse
+	return HttpResponse(json)
